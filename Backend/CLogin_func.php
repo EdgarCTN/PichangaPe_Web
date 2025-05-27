dd0e6e8d359bdcdf7ec56ba931be42168fb3234c
Daniel Tintaya <132936626+Gen54cw@users.noreply.github.com>
2025-05-27 22:04:25 +0000

    Creacion de la logica de Login para facilitar pruebas

---
+++ Backend/CLogin_func.php
@@ -0,0 +1,14 @@
+<?php
+function login($conexion, $usuario, $password) {
+    $query = "SELECT * FROM clientes 
+              WHERE usuario='$usuario' 
+                AND password='$password' 
+                AND rol='dueño'";
+    $resultado = mysqli_query($conexion, $query);
+
+    if ($resultado && $resultado->num_rows > 0) {
+        return mysqli_fetch_assoc($resultado);
+    } else {
+        return ["error" => "Credenciales incorrectas o acceso denegado"];
+    }
+}
