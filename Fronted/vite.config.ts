66240ba169d13c738026ed65753adebbf1ff7054
EdgarCTN <144568983+EdgarCTN@users.noreply.github.com>
2025-05-30 10:58:08 -0500

    Ignorar commit

--- Fronted/vite.config.ts
+++ Fronted/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/PichangaPe_Web/Fronted/",
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  return {
+    base:
+      command === "build"
+        ? "/PichangaPe_Web/Fronted/" // base para producción (github pages)
+        : "/", // base para desarrollo local
+    plugins: [react()],
+  };
 });
