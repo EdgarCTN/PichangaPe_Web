143f5e11651314235e55df7ec2efa3ff52b5785d
Daniel Tintaya <132936626+Gen54cw@users.noreply.github.com>
2025-06-22 21:29:15 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,7 +1,14 @@
 { pkgs }: {
-	deps = [
-   pkgs.php82Packages.composer
-   pkgs.curl
-		pkgs.php82
-	];
+  deps = [
+    pkgs.wget
+    pkgs.curl
+    pkgs.php82
+    pkgs.php82Packages.composer
+    pkgs.php82Packages.xdebug
+  ];
+
+  env = {
+    PHP_INI_SCAN_DIR = ".:/etc/php";
+    XDEBUG_MODE = "coverage";
+  };
 }
